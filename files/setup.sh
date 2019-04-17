33ee605d792d5660d97b412ff9d0616d40b57c28
tomoyuki.murakami <muratomo@bequem.co.jp>
2019-04-18 01:26:55 +0900

    test circleci

--- files/setup.sh
+++ files/setup.sh
@@ -1,3 +1,9 @@
+#/bin/bash
+
+set -o pipefail
+
+# setting nginx
+cat <<EOF > /etc/yum.repos.d/nginx.repo
 [nginx-stable]
 name=nginx stable repo
 baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
@@ -11,3 +17,10 @@ baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
 gpgcheck=1
 enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
+EOF
+
+yum install -y nginx
+
+cp ./files/mu-web.conf /etc/nginx/conf.d/mu-web.conf
+
+/usr/sbin/nginx -t -c /etc/nginx/conf.d/mu-web.conf
