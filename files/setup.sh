eb23bac4ae1af46f0f50042e4297204a23e0dcd0
tomoyuki.murakami <muratomo@bequem.co.jp>
2019-04-18 01:50:19 +0900

    fix repo

--- files/setup.sh
+++ files/setup.sh
@@ -2,18 +2,21 @@
 
 set -o pipefail
 
+CENTOS_VERSION=7
+ARCH=`arch`
+
 # setting nginx
 cat <<EOF > /etc/yum.repos.d/nginx.repo
 [nginx-stable]
 name=nginx stable repo
-baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/centos/${CENTOS_VERSION}/${ARCH}/
 gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
 
 [nginx-mainline]
 name=nginx mainline repo
-baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/mainline/centos/${CENTOS_VERSION}/${ARCH}/
 gpgcheck=1
 enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
