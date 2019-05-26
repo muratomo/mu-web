a3a11fc42c3283a2b85cdad1cbb27e7d8980f933
muratomo <muratomo40424@gmail.com>
2019-05-27 00:33:47 +0900

    add sudo

--- files/setup.sh
+++ files/setup.sh
@@ -6,14 +6,14 @@ DEBIAN_NAME=stretch
 
 # setting nginx
 curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-cat <<EOF > /etc/apt/sources.list.d/nginx.list
+sudo cat <<EOF > /etc/apt/sources.list.d/nginx.list
 deb http://nginx.org/packages/debian/ ${DEBIAN_NAME} nginx
 deb-src http://nginx.org/packages/debian/ ${DEBIAN_NAME} nginx
 EOF
 
-apt update
-apt remove -y nginx && apt install -y nginx
+sudo apt update
+sudo apt remove -y nginx && sudo apt install -y nginx
 
-cp ./files/mu-web.conf /etc/nginx/conf.d/mu-web.conf
+sudo cp ./files/mu-web.conf /etc/nginx/conf.d/mu-web.conf
 
-/usr/sbin/nginx -t
+sudo /usr/sbin/nginx -t
