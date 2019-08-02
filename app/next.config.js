4918f86ad3d97a1d775c4ae3990e443e984866f6
muratomo <muratomo40424@gmail.com>
2019-08-03 01:17:35 +0900

    setup next.js

---
+++ app/next.config.js
@@ -0,0 +1,11 @@
+const withSass = require('@zeit/next-sass')
+module.exports = withSass({
+  /* config options here */
+  exportPathMap: () => {
+    return {
+      '/': {
+        page: '/'
+      }
+    }
+  }
+})
