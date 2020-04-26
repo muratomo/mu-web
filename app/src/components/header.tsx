786bbdbe6a1dcba85f2754bdcb6d6e50d1daae9a
muratomo <muratomo40424@gmail.com>
2020-04-26 22:11:43 +0900

    add header.png

--- app/src/components/header.tsx
+++ app/src/components/header.tsx
@@ -3,9 +3,8 @@ import * as React from 'react';
 export default class Header extends React.Component<{}> {
   render(): JSX.Element {
     return (
-      <div>
-        <h1 id="title">Mu Web</h1>
-        <p style={{ 'textAlign': 'center' }}>サイト構築中。。。</p>
+      <div id="title">
+        <h1>Mu Web</h1>
       </div>
     );
   }
