008eebaac46f5c9545c2bf720d855895312cecf8
muratomo <muratomo40424@gmail.com>
2020-05-08 16:42:10 +0900

    fix lint

--- app/src/pages/design/index.tsx
+++ app/src/pages/design/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Link from 'next/link';
 import { NextPageContext } from 'next';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
