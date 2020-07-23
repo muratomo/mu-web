56346752e4ad15a0a52bba30ef571ca8a156092d
muratomo <muratomo40424@gmail.com>
2020-07-23 15:50:35 +0900

    fix export setting & nginx try_files

--- app/next.config.js
+++ app/next.config.js
@@ -2,6 +2,7 @@ const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
   /* config options here */
+  exportTrailingSlash: true,
   exportPathMap: () => {
     return {
       '/': { page: '/' },
