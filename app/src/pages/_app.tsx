134e65315243b139e66169b70c11c046908afc5b
muratomo <muratomo40424@gmail.com>
2020-12-27 14:12:11 +0900

    make top page

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '../style/index.scss';
 import { AppProps } from 'next/app';
 import * as React from 'react';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
+
+export default App;
