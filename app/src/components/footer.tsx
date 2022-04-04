ad3b91bd1c697cc7c21712bd84830900aaae488d
muratomo <muratomo40424@gmail.com>
2022-04-04 23:06:54 +0900

    upgrade nextjs@12 + add prettier

--- app/src/components/footer.tsx
+++ app/src/components/footer.tsx
@@ -1,15 +1,17 @@
-import * as React from 'react';
+import * as React from 'react'
 
 class Footer extends React.Component<unknown> {
-  private fullYear: number = new Date().getFullYear();
+  private fullYear: number = new Date().getFullYear()
 
   render(): JSX.Element {
     return (
-      <footer className="mu-footer">
-        <span className="mu-footer__copyright">&copy; {this.fullYear} muratomo</span>
+      <footer className='mu-footer'>
+        <span className='mu-footer__copyright'>
+          &copy; {this.fullYear} muratomo
+        </span>
       </footer>
-    );
+    )
   }
 }
 
-export default Footer;
+export default Footer
